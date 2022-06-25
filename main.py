35bcf8fed7b9775583c0b73d94dc930d273577dc
rayct <rayct@icloud.com>
2022-06-25 15:42:49 +0100

    Project Completed

--- main.py
+++ main.py
@@ -22,12 +22,76 @@ class PasswordManager:
 
         if initial_values is not None:
             for key, value in initial_values.items():
-                pass # TODO: add password function
+                self.add_password(key, value)
 
-    def load_password_file(selfself, path):
+    def load_password_file(self, path):
         self.password_file = path
 
         with open(path, 'r') as f:
             for line in f:
                 site, encrypted = line.split(':')
-                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode
+                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
+
+    def add_password(self, site, password):
+        self.password_dict[site] = password
+
+        if self.password_file is not None:
+            with open(self.password_file, 'a+') as f:
+                encrypted = Fernet(self.key).encrypt(password.encode())
+                f.write(site + ':' + encrypted.decode() + '\n')
+
+    def get_password(self, site):
+        return self.password_dict[site]
+
+
+def main():
+    password = {
+        'email': '1234567',
+        'facebook': 'helloworld123',
+        'youtube': 'myfavouritepassword_123',
+        'something': 'mysomethingpassword'
+    }
+
+    pm = PasswordManager()
+
+    print('''What do you wan/'t to do..?
+    (1) Creat a new key
+    (2) Load an existing
+    (3) Create a new password file
+    (4) Load existing password file
+    (5) Add a new password
+    (6) Get a password
+    (q) Quit
+    ''')
+
+    done = False
+
+    while not done:
+        choice = input('Enter your choice; ')
+        if choice == '1':
+            path = input('Enter path: ')
+            pm.create_key(path)
+        elif choice == '2':
+            path = input('Enter path: ')
+            pm.load_key(path)
+        elif choice == '3':
+            path = input('Enter path: ')
+            pm.create_password_file(path, password)
+        elif choice == '4':
+            pm.load_password_file(path)
+        elif choice == '5':
+            site = input('Enter the site: ')
+            password = input('Enter the password: ')
+            pm.add_password(site, password)
+        elif choice == '6':
+            site = input('What site do you wan\'t..?: ')
+            print(f'Password for {site} is {pm.get_password(site)}')
+        elif choice == 'q':
+            done = True
+            print('Goodbye')
+        else:
+            print('Invalid Choice!')
+
+
+if __name__ == '__main__':
+    main()
