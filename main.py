e5c411b4f924a61cc8cff6aa0cacec5d0eca0371
rayct <rayct@icloud.com>
2022-06-25 02:33:13 +0100

    Implemented Decrypt code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class PasswordManager:
 
     def __init__(self):
         self.key = None
-        self.paswword_file = None
+        self.password_file_file = None
         self.password_dict = {}
 
     def create_key(self, path):
@@ -16,3 +16,18 @@ class PasswordManager:
     def load_key(self, path):
         with open(path, 'rb') as f:
             self.key = f.read()
+
+    def create_password_file(self, path, initial_values=None):
+        self.password_file = path
+
+        if initial_values is not None:
+            for key, value in initial_values.items():
+                pass # TODO: add password function
+
+    def load_password_file(selfself, path):
+        self.password_file = path
+
+        with open(path, 'r') as f:
+            for line in f:
+                site, encrypted = line.split(':')
+                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode
